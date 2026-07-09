aa189d8492c25b2cf109166b0cf1767fb2721b8f
lop <61240663+loplxl@users.noreply.github.com>
2026-07-09 19:49:48 +0100

    change thorium
    thorium download now scans for the latest binary release

--- Source Code/downloads/chromium/thorium.py
+++ Source Code/downloads/chromium/thorium.py
@@ -1,3 +1,21 @@
+import aiohttp
 from pathlib import Path
 async def getURL(ssl_ctx,continuation,progressbar,completeDownload):
-    await continuation("https://github.com/Alex313031/Thorium-Win/releases/latest/download/thorium_AVX2_mini_installer.exe",(Path.home() / "Downloads" / "thorium_AVX2_mini_installer.exe"))
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.github.com/repos/Alex313031/Thorium-Win/releases",ssl=ssl_ctx) as resp:
+                resp.raise_for_status()
+                releases = await resp.json()
+    except Exception as e:
+        print(f"Error during download: {e}")
+        await completeDownload(progressbar,progressbar.master,msg="Error",text_color="#ff5555")
+        return
+    url = None
+    for release in releases:
+        for assets in release.get("assets", []):
+            if assets["name"].endswith("thorium_AVX2_mini_installer.exe"):
+                url = assets["browser_download_url"]
+                break
+        if url:
+            break
+    await continuation(url,(Path.home() / "Downloads" / f"{url.rsplit("/",1)[1]}"))
